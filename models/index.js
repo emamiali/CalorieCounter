70794d9091dba020cd840c14d2a48c4fda30be18
Ali Emami <emamiali1993@gmail.com>
2017-07-25 10:58:30 -0700

    another attempt at heroku

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 MONGOLAB_URI = 'mongodb://admin:admin@ds123193.mlab.com:23193/calories_counter'
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/CaloriesCounter');
+mongoose.connect('mongodb://admin:admin@ds123193.mlab.com:23193/calories_counter' || 'mongodb://localhost/CaloriesCounter');
 
 
 module.exports = {

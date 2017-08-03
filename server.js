45ad3a568af5484a6b31e693ccda20435343271b
Ali Emami <emamiali1993@gmail.com>
2017-08-02 18:20:39 -0700

    added the nutrionix api to the user and meal page

--- server.js
+++ server.js
@@ -23,23 +23,6 @@ app.use(logger('dev'));
 
 app.use(cors());
 
-// function createCORSRequest(methos, url) {
-//   var xhr = new XMLHttpRequest();
-//   if ("withCredentials" in xhr) {
-//     xhr.open(method, url, true);
-//   } else if (typeof XDomainRequest != "undefined") {
-//     xhr = new XDomainRequest();
-//     xhr.open(method, url);
-//   }else {
-//     xhr = null;
-//   }
-//   return xhr;
-// }
-//
-// var xhr = createCORSRequest('GET', url);
-// if(!xhr) {
-//   throw new Error('Cors not supported');
-// }
 //===============
 //== Auth Routes
 //===============
